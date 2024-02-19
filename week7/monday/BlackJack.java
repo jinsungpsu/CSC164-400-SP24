57c4191d7fc50f1c28bc0e4b082b89f8b39bc5bb
Jin An <jin.an@dtcc.edu>
2024-02-19 14:25:24 -0500

    Create BlackJack.java

---
+++ week7/monday/BlackJack.java
@@ -0,0 +1,18 @@
+public class Exam1Review {
+    public static void main(String[] args) {
+        // cards array represents
+        // the dealers cards
+        // and my cards
+        // first dimension = dealer and me
+        // second dimension is cards...
+        int cards[][] = {
+                {5, 4, 3, 10},
+                {5, 6, 5, 5}
+        };
+
+        // print out all the cards from both dealer and player
+        // print out the sum for each
+        // print blackjack if sum is 21
+
+    }
+}
